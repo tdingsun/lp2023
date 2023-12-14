03c5e038b4ec1f2e5d4866a8ecfa2d4914a41a4d
tdingsun <tiger.dingsun@gmail.com>
2023-12-14 03:09:12 -0500

    styling

--- src/lib/components/SongTitle.svelte
+++ src/lib/components/SongTitle.svelte
@@ -6,7 +6,7 @@
 
 <div class="text-2xl tracking-tighter leading-6 border-b border-grey6 min-h-[calc(2rem-1px)] flex items-center i">
 	<p>
-		{song.songname} – <i>{song.artistname}</i>
+		<span class="inline-block">{song.songname}</span> <span class="inline-block"><i>– {song.artistname}</i></span>
 	</p>
 </div>
 
