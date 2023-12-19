3c89b81cb7919e0121d74febf868fc9c8749e88a
tdingsun <tiger.dingsun@gmail.com>
2023-12-19 11:02:58 -0500

    no rounded corners

--- src/lib/components/Page.svelte
+++ src/lib/components/Page.svelte
@@ -7,7 +7,7 @@
 <div
 	class="w-full max-w-[calc((100vh-4rem)*0.773)] h-0 pb-[clamp(0px,129.4%,calc(100vh-4rem))] relative mx-auto"
 >
-	<div class=" absolute left-0 right-0 top-0 bottom-0 flex flex-col bg-[#f5f4ee] rounded-sm">
+	<div class=" absolute left-0 right-0 top-0 bottom-0 flex flex-col bg-[#f5f4ee]">
 		{#if entry}
 			<h1 class="text-grey6 text-4xl font-serif text-center pt-1 z-10">{entry.name}</h1>
 			<hr class="border-grey6 mx-4 -mt-2" />
