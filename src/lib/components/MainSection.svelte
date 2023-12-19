091d683fdc18f5ee42dd28b1006e0eacdaaa2bd1
tdingsun <tiger.dingsun@gmail.com>
2023-12-19 09:50:42 -0500

    add new background

--- src/lib/components/MainSection.svelte
+++ src/lib/components/MainSection.svelte
@@ -1,12 +1,12 @@
 <script>
-	import BackgroundImg from '$lib/images/background.jpg';
+	import BackgroundImg from '$lib/images/background.png';
 	import MusicStand from '$lib/images/musicstand.svg';
 	import Pencil from '$lib/images/pencil.svg';
 </script>
 
 	<div
 		style="background-image: url({BackgroundImg})"
-		class="flex-grow basis-1/2 h-full -ml-1 pr-8 pt-8 pl-9 pb-[1%] flex justify-center items-end relative"
+		class="bg-cover bg-center flex-grow basis-1/2 h-full -ml-1 pr-8 pt-8 pl-9 pb-[1%] flex justify-center items-end relative"
 	>
 		<div>
 			<img src={MusicStand} class="w-full absolute bottom-0 left-0 right-0"/>
