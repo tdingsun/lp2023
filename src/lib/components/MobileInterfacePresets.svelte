fa3574c5463fd8fee0c0100e384086eb8bade470
tdingsun <tiger.dingsun@gmail.com>
2023-12-21 13:59:41 -0500

    typo

--- src/lib/components/MobileInterfacePresets.svelte
+++ src/lib/components/MobileInterfacePresets.svelte
@@ -45,7 +45,7 @@
 		LP [Long Play, Listening Party, Little Puppies…] was formed in 2018 as a college radio show, music club, and IRL zine project. today, it exists as an open-ended publishing platform dedicated to celebrating our personal connections to the music of our lives.
 	</p>
 	<p>
-		this third edition, LP-2023, is a collaboration between tiger dingsun, kevin dong, and all the Lovely People who submitted their SOTYs… thank you all!”
+		this third edition, LP-2023, is a collaboration between tiger dingsun, kevin dong, and all the Lovely People who submitted their SOTYs… thank you all!
 	</p>
 </div>
 {/if}
