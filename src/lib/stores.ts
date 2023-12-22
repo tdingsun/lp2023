a235d4ee4c1f33f692e7f2bd9f31e5220889eb19
tdingsun <tiger.dingsun@gmail.com>
2023-12-22 18:17:30 -0500

    code cleanup

--- src/lib/stores.ts
+++ src/lib/stores.ts
@@ -1,10 +1,9 @@
-import { derived, get, writable } from "svelte/store";
-import { type SongEntry, type Entry } from "./types";
-import { goto } from "$app/navigation";
+import { derived, get, writable } from 'svelte/store';
+import { type SongEntry, type Entry } from './types';
+import { goto } from '$app/navigation';
 
-export const currentEmbedCode = writable<string|undefined>();
-export const currentEntry = writable<Entry|undefined>();
-export const currentSong = writable<SongEntry|undefined>();
+export const currentEntry = writable<Entry | undefined>();
+export const currentSong = writable<SongEntry | undefined>();
 export const YTplayer = writable<any>();
 export const SCplayer = writable<any>();
 export const SCfirstPlay = writable<boolean>(true);
@@ -14,50 +13,52 @@ export const isYTPlayerPlaying = writable<boolean>(false);
 
 export const entries = writable<Entry[] | undefined>();
 
-export const setCurrentSong = (embedCode: string|undefined) => {
-    let entry = get(currentEntry);
-    if(entry){
-        let currSong = entry.songs.find((song) => song.embedCode === embedCode)
-        currentSong.set(currSong)
-    }
-}
+export const setCurrentSong = (embedCode: string | undefined) => {
+	let entry = get(currentEntry);
+	if (entry) {
+		let currSong = entry.songs.find((song) => song.embedCode === embedCode);
+		currentSong.set(currSong);
+	}
+};
 
-export const isMusicPlaying = derived([isSCPlayerPlaying, isYTPlayerPlaying], ([$isSCPlayerPlaying, $isYTPlayerPlaying]) => {
-    return ($isSCPlayerPlaying || $isYTPlayerPlaying)
-})
+export const isMusicPlaying = derived(
+	[isSCPlayerPlaying, isYTPlayerPlaying],
+	([$isSCPlayerPlaying, $isYTPlayerPlaying]) => {
+		return $isSCPlayerPlaying || $isYTPlayerPlaying;
+	}
+);
 
-export const currentSongIsSC = derived(currentSong, ($currentSong) => $currentSong?.embedCode?.includes('soundcloud'));
+export const currentSongIsSC = derived(
+	currentSong,
+	($currentSong) => $currentSong?.embedCode?.includes('soundcloud')
+);
 
 export const chooseRandomSong = () => {
-    let allEntries = get(entries);
-    if(allEntries && allEntries.length > 0){
-        let randomIdx = Math.floor((Math.random() * allEntries.length))
-        let randomEntry = allEntries[randomIdx];
-        let slug = randomEntry.slug;
-        let randomSongIdx =  Math.floor((Math.random() * randomEntry.songs.length))
-        let randomSong = randomEntry.songs[randomSongIdx]
-        goto(`/${slug}`).then(() => {
-            setCurrentSong(randomSong.embedCode);
-            let songElement = document.getElementById(`${slug}-${randomSongIdx}`);
-            if(songElement){
-                songElement.scrollIntoView();
-                songElement.style.transition = '0.5s';
+	let allEntries = get(entries);
+	if (allEntries && allEntries.length > 0) {
+		let randomIdx = Math.floor(Math.random() * allEntries.length);
+		let randomEntry = allEntries[randomIdx];
+		let slug = randomEntry.slug;
+		let randomSongIdx = Math.floor(Math.random() * randomEntry.songs.length);
+		let randomSong = randomEntry.songs[randomSongIdx];
+		goto(`/${slug}`).then(() => {
+			setCurrentSong(randomSong.embedCode);
+			let songElement = document.getElementById(`${slug}-${randomSongIdx}`);
+			if (songElement) {
+				songElement.scrollIntoView();
+				songElement.style.transition = '0.5s';
 
-                songElement.style.boxShadow = '0 0 20px 5px #ffb448';
-                songElement.style.backgroundColor = '#ffb448AA';
-                setTimeout(() => {
-                    songElement.style.boxShadow = '';
-                    songElement.style.backgroundColor = '';
-                }, 2000);
-            }
-            let pianoElement = document.getElementById(`entry-${randomIdx}`);
-            if(pianoElement){
-                pianoElement.scrollIntoView();
-            }
-        }
-
-        )
-
-
-    }
-}
+				songElement.style.boxShadow = '0 0 20px 5px #ffb448';
+				songElement.style.backgroundColor = '#ffb448AA';
+				setTimeout(() => {
+					songElement.style.boxShadow = '';
+					songElement.style.backgroundColor = '';
+				}, 2000);
+			}
+			let pianoElement = document.getElementById(`entry-${randomIdx}`);
+			if (pianoElement) {
+				pianoElement.scrollIntoView();
+			}
+		});
+	}
+};
